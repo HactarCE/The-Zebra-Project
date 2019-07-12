e60846329c084cd55d4e105aecf93108b2abcbe9
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-07-12 11:20:57 -0400

    Remove duplicate lines automatically

--- sort.py
+++ sort.py
@@ -2,7 +2,7 @@
 
 zebra_lines = []
 with open('zebra.txt') as f:
-    zebra_lines = sorted(f, key=lambda s: s.casefold())
+    zebra_lines = sorted(set(f), key=lambda s: s.casefold())
 with open('zebra.txt', 'w') as f:
     for line in zebra_lines:
         f.write(line)
